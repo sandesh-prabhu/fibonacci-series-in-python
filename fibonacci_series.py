000ed94aa9eb76a37834831db00757cbc207384c
sandesh-prabhu <77835063+sandesh-prabhu@users.noreply.github.com>
2021-04-20 00:21:49 +0530

    Create fibonacci_series.py

---
+++ fibonacci_series.py
@@ -0,0 +1,17 @@
+while 1:
+    def fib(n):
+        if n==0 or n==1:
+            return n
+        else:
+            return fib(n-1)+fib(n-2)
+    num=input("Enter positive number(number of terms)(enter nothing to quit):")
+    if num=="":
+        break
+    elif not num.isnumeric() or int(num)<0:
+        print("enter a positive integer")
+    elif num=="":
+        break
+    else:
+        print("Fibonacci series:")
+        for i in range(int(num)):
+            print(fib(i))
